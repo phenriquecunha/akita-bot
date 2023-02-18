1b1e5255e6f9ad4b10d78a5de675812e49df8231
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 20:31:31 -0300

    links command

--- src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/slack")
 public class AkitaController {
   @PostMapping("/interactivity")
-  public ResponseEntity<Object> InteractiveComponents(){
-    return ResponseEntity.ok().body(new Object(){String text = "lista de links";});
+  public ResponseEntity<Object> InteractiveComponents(Object object) {
+    return ResponseEntity.ok().body(new Object() {
+      String text = "lista de links";
+    });
   }
+
   @PostMapping("/slash")
-  public ResponseEntity<Object> slashCommands(){
-    return ResponseEntity.ok().body(new Object(){String text = "lista de links";});
+  public ResponseEntity<Object> slashCommands(Object object) {
+    return ResponseEntity.ok().body(new Object() {
+      String text = "lista de links";
+      String response_type = "in_channel";
+    });
   }
 }
