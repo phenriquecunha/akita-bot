4c94407a82145adedf7711492ddad812258d7093
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 18:48:38 -0300

    fix deploy

--- src/main/java/com/techbivix/slackbot/akita/controllers/EntryController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/EntryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class EntryController {
-  @GetMapping
+  @GetMapping()
   ResponseEntity<String> home(){
     return ResponseEntity.ok().body("Tá funfando fiote!");
   }
