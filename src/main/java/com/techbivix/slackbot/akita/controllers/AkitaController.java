1cbafa8ca4fad83debc5f61de3575d8d39ae09a3
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 20:07:14 -0300

    links command

--- src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
@@ -1,8 +1,13 @@
 package com.techbivix.slackbot.akita.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/slack")
 public class AkitaController {
-
+  @PostMapping("interactivity")
+  public ResponseEntity<Object> InteractiveComponents(){
+    return ResponseEntity.ok().body("'links': ['bla','bla']");
+  }
 }
--- src/main/java/com/techbivix/slackbot/akita/services/WebHook.java
+++ src/main/java/com/techbivix/slackbot/akita/services/WebHook.java
@@ -1,8 +1,11 @@
 package com.techbivix.slackbot.akita.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("https://hooks.slack.com/services/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")
-public class WebHook {
+@FeignClient("https://hooks.slack.com/services/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")
+public interface WebHook {
+  @PostMapping("/")
+  public String alert();
 
 }
