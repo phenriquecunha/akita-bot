7776b1980ca4029a5157825feb77cd00ab64d529
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 18:29:32 -0300

    fix route

--- src/main/java/com/techbivix/slackbot/akita/controllers/EntryController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/EntryController.java
@@ -4,9 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/")
 public class EntryController {
-  @GetMapping("/")
+  @GetMapping
   ResponseEntity<String> home(){
     return ResponseEntity.ok().body("Tá funfando fiote!");
   }
