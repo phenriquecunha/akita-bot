b6b2e041f35719a2df0863c720187550c7bdbd5c
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 21:37:11 -0300

    links command

--- src/main/java/com/techbivix/slackbot/akita/AkitaApplication.java
+++ src/main/java/com/techbivix/slackbot/akita/AkitaApplication.java
@@ -2,8 +2,10 @@ package com.techbivix.slackbot.akita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
 public class AkitaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
@@ -1,23 +1,25 @@
 package com.techbivix.slackbot.akita.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/slack")
+@RestController
+@RequestMapping("/slack")
 public class AkitaController {
   @PostMapping("/interactivity")
-  public ResponseEntity<Object> InteractiveComponents(Object object) {
+  public ResponseEntity<Object> InteractiveComponents(@RequestBody Object object) {
     return ResponseEntity.ok().body(new Object() {
-      String text = "lista de links";
+      public final String text = "lista de links";
     });
   }
 
   @PostMapping("/slash")
-  public ResponseEntity<Object> slashCommands(Object object) {
-    return ResponseEntity.ok().body(new Object() {
-      String text = "lista de links";
-      String response_type = "in_channel";
+  public ResponseEntity<Object> slashCommands(@RequestBody Object object) {
+    return ResponseEntity.status(200).body(new Object() {
+      public final Object text = "lista de links";
+      public final Object response_type = "in_channel";
     });
   }
+
 }
