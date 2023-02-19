7d36c7949bd9e9590218ee4d5beb26a8d382447b
Pedro Cunha <phcs-12@hotmail.com>
2023-02-18 22:38:56 -0300

    links command

--- src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
+++ src/main/java/com/techbivix/slackbot/akita/controllers/AkitaController.java
@@ -1,24 +1,22 @@
 package com.techbivix.slackbot.akita.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/slack")
 public class AkitaController {
-//  @PostMapping("/interactivity")
-//  public ResponseEntity<Object> InteractiveComponents(@RequestBody Object object) {
-//    return ResponseEntity.ok().body(new Object() {
-//      public final String text = "lista de links";
-//    });
-//  }
+  @PostMapping("/interactivity")
+  public ResponseEntity<Object> interactiveComponents(@RequestBody Object object) {
+    return ResponseEntity.ok().body(new Object() {
+      public final String text = "lista de links";
+    });
+  }
 
-
-  @PostMapping(value = "/**",
+  @PostMapping(value = "/slash",
   consumes = {"application/x-www-form-urlencoded;charset=UTF-8"})
   public ResponseEntity<Object> slashCommands(Object object){
-    return ResponseEntity.status(200).body(new Object() {
+    return ResponseEntity.ok().body(new Object() {
       public final Object response_type = "in_channel";
       public final Object text = "lista de links";
     });
